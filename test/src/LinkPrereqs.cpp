acb1cdfc0e0ce0be594d7d318ac900e238237878
Olivia <zoe.i2k1@gmail.com>
2021-03-01 23:29:10 +0100

    Fuck you Windows

--- test/src/LinkPrereqs.cpp
+++ test/src/LinkPrereqs.cpp
@@ -10,5 +10,6 @@ TEST_CASE("Verify getExecutableLocation()", "[ExecutableLocation]") {
     INFO("getExecutableLocation() didn't throw, received " + s);
     // Sanitize newlines?
     REQUIRE(!std::regex_search(s, std::regex("(\n|\r)+")));
-    REQUIRE(std::regex_match(s, std::regex("([a-zA-Z]:)?[\\/].*[\\/]build[\\/]test[\\/]bin[\\/]tests(.exe)?")));
+    REQUIRE(std::regex_match(
+            s, std::regex("([a-zA-Z]:)?[\\\\/].*[\\\\/]build[\\\\/]test[\\\\/]bin[\\\\/]tests(.exe)?")));
 }
