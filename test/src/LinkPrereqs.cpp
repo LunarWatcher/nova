cf6e5c8bf20aaf5b7b4d706d937768941d1f35c6
Olivia <zoe.i2k1@gmail.com>
2021-03-01 23:15:26 +0100

    Try to narrow down the Windoze problem

--- test/src/LinkPrereqs.cpp
+++ test/src/LinkPrereqs.cpp
@@ -8,5 +8,7 @@ TEST_CASE("Verify getExecutableLocation()", "[ExecutableLocation]") {
     std::string s;
     REQUIRE_NOTHROW(s = termutil::Filesystem::getExecutableLocation());
     INFO("getExecutableLocation() didn't throw, received " + s);
+    // Sanitize newlines?
+    REQUIRE(!std::regex_search(s, std::regex("(\n|\r)+")));
     REQUIRE(std::regex_match(s, std::regex("([a-zA-Z]:)?[\\/].*[\\/]build[\\/]test[\\/]bin[\\/]tests(.exe)?")));
 }
